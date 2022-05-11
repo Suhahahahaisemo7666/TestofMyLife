78d58eb2997d0b6f7730cc85eda67d15570479eb
Suhahahahaisemo7666 <105345405+Suhahahahaisemo7666@users.noreply.github.com>
2022-05-11 17:18:17 +1000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var  number ;
+
+function setup() {
+  createCanvas(800,800);
+  number=createSprite(200,200,10,30);
+}
+
+function draw() 
+{
+  background("lavender");
+text (mouseX+","+mouseY,mouseX, mouseY)
+drawSprites()
+}
+
+
+
+
